9f3e9d3e94f295173360a1919dec758c57a4e166
Suzu Nagano <sszz12121994@gmail.com>
2021-07-30 21:51:33 +0900

    FIx:⚡ meta修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
-      { hid: 'description', name: 'description', content: 'PADIオープンウォーター試験対策のクイズです' }
+      { hid: 'description', name: 'description', content: 'PADIオープンウォーター試験対策のクイズです' },
+      { hid: 'og:url', property: 'og:url', content: 'https://padi-open-water.netlify.app/' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'PADI オープンウォーター試験対策クイズ' },
+      { hid: 'og:description', property: 'og:description', content: 'PADIオープンウォーター試験対策のクイズです' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
